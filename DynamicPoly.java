5d0658e90fa54f649372288974dfb9bf025097ed
Sham <122438949+shammy14-1@users.noreply.github.com>
2024-04-28 16:22:28 +0800

    Update DynamicPoly.java

--- DynamicPoly.java
+++ DynamicPoly.java
@@ -1,6 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-AKO SI RHENZZZZZZZ CRUSH KO SI
+// malaki burat ko 
 
 public class DynamicPoly  {
     public static void main(String[] args) throws Exception {
